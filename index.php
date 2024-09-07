46671f2829fb42bea716b41c6f70c3c01b51f295
Renxdra <narendrawibowo336@gmail.com>
2024-09-07 17:42:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annahl Narendra Form</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+</head>
+<body>
+    <section class="contact">
+        <div class="content">
+            <h2>Universitas Brawijaya Center</h2>
+            <p>"Universitas Brawijaya Center adalah pusat inovasi dan riset terkemuka yang bertujuan untuk mendukung pengembangan ilmu pengetahuan dan teknologi, serta berkontribusi pada kemajuan masyarakat melalui penelitian berkualitas dan kolaborasi dengan berbagai mitra di tingkat nasional dan internasional."</p>
+        </div>
+        <div class="container">
+            <div class="contactInfo">
+                <div class="box">
+                    <div class="icon"><i class="fas fa-map-marker" aria-hidden="true"></i></div>
+                    <div class="text">
+                        <h3>Alamat</h3>
+                        <p>Jalan Veteran Malang</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i class="fas fa-envelope" aria-hidden="true"></i></div>
+                    <div class="text">
+                        <h3>Email</h3>
+                        <p>rendratampan123@gmail.com</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="icon"><i class="fas fa-phone" aria-hidden="true"></i></div>
+                    <div class="text">
+                        <h3>No HP</h3>
+                        <p>05535677788</p>
+                    </div>
+                </div>
+            </div>
+            <div class="contactForm">
+                <form action="proses.php" method="post">
+                    <h2>Masukan</h2>
+                    <div class="inputBox">
+                        <input type="text" name="name" required="required">
+                        <span>Nama Lengkap</span>
+                    </div>
+                    <div class="inputBox">
+                        <input type="text" name="email" required="required">
+                        <span>Email</span>
+                    </div>
+                    <div class="inputBox">
+                        <textarea name="message" required="required"></textarea>
+                        <span>Masukan Pesan Anda ....</span>
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" value="send">
+                    </div>
+                </form>
+                <?php if (isset($_GET['notif'])) : ?>
+                    <?php if ($_GET['notif'] == 'kirimberhasil') : ?>
+                        <div class="alert">Pesan Berhasil Dikirim</div>
+                    <?php endif ?>
+                    <?php if ($_GET['notif'] == 'kirimgagal') : ?>
+                        <div class="alert">Pesan Gagal Dikirim</div>
+                    <?php endif ?> 
+                <?php endif ?>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ rendra.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 30, 2024 at 06:58 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rendra`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formcontact`
+--
+
+CREATE TABLE `formcontact` (
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `message` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `formcontact`
+--
+
+INSERT INTO `formcontact` (`name`, `email`, `message`) VALUES
+('asa', 'as@mm.com', 'a');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
